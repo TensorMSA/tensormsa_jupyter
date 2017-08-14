1fe895165fcce955c1523c3dd1a7d556c0e673ad
youngjaekim0129 <youngjaekim@poscoict.com>
2017-08-15 00:30:30 +0900

    add preprocess

---
+++ chap06_image/hdf5.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import numpy as np
+import h5py
+
+image_arr = []
+shape_arr = []
+lable_arr = []
+img = Image.open('/tmp/resize.jpeg')
+img = np.array(img)
+shape_arr.append(img.shape)
+img = img.flatten()
+image_arr.append(img)
+lable_arr.append('elephant'.encode('utf8'))
+
+output_path = '/tmp/hdf5'
+h5file = h5py.File(output_path, mode='w')
+dtype = h5py.special_dtype(vlen=np.dtype('uint8'))
+hdf_features = h5file.create_dataset('image_features', (1,), dtype=dtype)
+hdf_shapes = h5file.create_dataset('image_features_shapes', (1, 3), dtype='int32')
+hdf_labels = h5file.create_dataset('targets', (1,), dtype='S240')
+
+# Attach shape annotations and scales
+hdf_features.dims.create_scale(hdf_shapes, 'shapes')
+hdf_features.dims[0].attach_scale(hdf_shapes)
+
+hdf_shapes_labels = h5file.create_dataset('image_features_shapes_labels', (3,), dtype='S7')
+hdf_shapes_labels[...] = ['channel'.encode('utf8'),
+                          'height'.encode('utf8'),
+                          'width'.encode('utf8')]
+hdf_features.dims.create_scale(hdf_shapes_labels, 'shape_labels')
+hdf_features.dims[0].attach_scale(hdf_shapes_labels)
+
+# Add axis annotations
+hdf_features.dims[0].label = 'batch'
+
+for i in range(len(image_arr)):
+    hdf_features[i] = image_arr[i]
+    hdf_shapes[i] = shape_arr[i]
+    hdf_labels[i] = lable_arr[i]
+
+h5file.flush()
+h5file.close()
+
+h5file = h5py.File(output_path, mode='r')
+rawdata = h5file['image_features']
+targets = h5file['targets']
+print()
--- chap06_image/preprocess/img_to_hdf5.py
+++ chap06_image/preprocess/img_to_hdf5.py
@@ -3,11 +3,12 @@ import h5py
 import numpy as np
 from PIL import Image
 
-directory = '/tmp/air_car'
+directory = '/air_car'
 folderlist = os.listdir(directory)
 folderlist.sort()
 filecnt = 0
 image_arr = []
+shape_arr = []
 lable_arr = []
 processcnt = 1
 
@@ -35,11 +36,13 @@ for folder in folderlist:
             img = img.resize((32, 32), Image.ANTIALIAS)
             img.save(directory + '/' + str(filecnt) + '.jpg')
             img = np.array(img)
+            shape_arr.append(img.shape)
             img = img.reshape([-1, 32, 32, 3])
             img = img.flatten()
 
             image_arr.append(img)
             lable_arr.append(folder.encode('utf8'))
+            print(str(len(shape_arr))+' '+str(len(image_arr)))
             filecnt += 1
 
             print("Processcnt=" + str(processcnt) + " File=" + directory + " forder=" + folder + "  name=" + filename)
@@ -50,7 +53,7 @@ for folder in folderlist:
         processcnt += 1
 
     if filecnt > 0:
-        output_path = '/tmp/air_car/hdf5'
+        output_path = '/air_car/hdf5'
         h5file = h5py.File(output_path, mode='w')
         dtype = h5py.special_dtype(vlen=np.dtype('uint8'))
         hdf_features = h5file.create_dataset('image_features', (filecnt,), dtype=dtype)
@@ -73,6 +76,7 @@ for folder in folderlist:
 
         for i in range(len(image_arr)):
             hdf_features[i] = image_arr[i]
+            #hdf_shapes[i] = shape_arr[i]
             hdf_labels[i] = lable_arr[i]
 
         h5file.flush()
