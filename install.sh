6015bfca974224595c82bba9a30f1128849611cc
Melon Musk <jeehyun100@users.noreply.github.com>
2017-11-28 13:11:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ git clone --recursive https://github.com/dmlc/xgboost
 cd xgboost; make -j4
 cd python-package; sudo python setup.py install
 
-http://xgboost.readthedocs.io/en/latest/build.html
-echo "export PYTHONPATH=${PYTHONPATH}/root/notebooks/xgboost/python-package" >> ~/.bashrc
-source ~/.bashrc
-
 apt-get install graphviz
 pip install seaborn 
 pip install plotly
